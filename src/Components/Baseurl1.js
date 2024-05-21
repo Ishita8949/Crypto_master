7aeadca9fe2db348c0fe5cd9ae416ee0136534b9
Ishita <ishumodi2507@gmail.com>
2024-05-21 19:07:45 +0530

    first commit

---
+++ src/Components/Baseurl1.js
@@ -0,0 +1,4 @@
+export const Baseurl1 = 'https://api.coingecko.com/api/v3'
+
+
+ 
---
+++ src/Components/baseUrl.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.coingecko.com/api/v3/exchanges?x_cg_demo_api_key=CG-GbqKvC1ihcNmZZtGoJKgopgV' 
+
+
+ 
